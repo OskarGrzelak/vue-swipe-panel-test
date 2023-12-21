71d255e3b2655e3093549015b284871547aedb3b
Oskar Grzelak <oskar.grzelak@smallgis.pl>
2023-12-21 11:28:55 +0100

    set height of a swipe panel to visual viewport height

--- src/components/SwipePanel.vue
+++ src/components/SwipePanel.vue
@@ -220,12 +220,14 @@ export default {
             parseInt(window.visualViewport.height) +
             "px, width=device-width, initial-scale=1.0"
         );
+        this.$refs.panel.style.height = `${window.visualViewport.height}px`;
       } else {
         const metaViewport = document.querySelector("meta[name=viewport]");
         metaViewport.setAttribute(
           "content",
           "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, height=device-height"
         );
+        this.$refs.panel.style.height = "";
       }
       this.$emit("resize", {
         initialHeight: this.initialHeight,
