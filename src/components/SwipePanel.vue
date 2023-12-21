0e3085f0bd2bfff292aa2bde4d6d96d699c89efd
Oskar Grzelak <oskar.grzelak@smallgis.pl>
2023-12-21 08:40:20 +0100

    test setting viewport attrs on resize

--- src/components/SwipePanel.vue
+++ src/components/SwipePanel.vue
@@ -209,6 +209,12 @@ export default {
   },
   methods: {
     setSwipePanel() {
+      document.documentElement.style.setProperty("overflow", "auto");
+      const metaViewport = document.querySelector("meta[name=viewport]");
+      metaViewport.setAttribute(
+        "content",
+        "height=" + initialHeight + "px, width=device-width, initial-scale=1.0"
+      );
       this.panelHeight = this.$refs.panel.offsetHeight;
       this.panelHeaderHeight = this.$refs.panelHeader.offsetHeight;
       this.createLevels();
